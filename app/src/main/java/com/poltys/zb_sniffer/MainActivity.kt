b0c7acbdf2e680cbd6a693c161bf09dbe5435c69
Alex Johnson <alexiionescu@gmail.com>
2024-05-28 13:33:32 +0000

    make GUI for StatsRCP

--- app/src/main/java/com/poltys/zb_sniffer/MainActivity.kt
+++ app/src/main/java/com/poltys/zb_sniffer/MainActivity.kt
@@ -7,37 +7,52 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Card
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.poltys.protos.zb_stats.StatsReportGrpcKt
+import com.poltys.protos.zb_stats.statsReply
+import com.poltys.protos.zb_stats.statsRequest
 import com.poltys.zb_sniffer.ui.theme.Zb_snifferTheme
-
 import io.grpc.ManagedChannelBuilder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.asExecutor
-import java.io.Closeable
-
-import com.poltys.protos.zb_stats.*
 import kotlinx.coroutines.launch
+import java.io.Closeable
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
 
 class MainActivity : ComponentActivity() {
-    private val uri by lazy {
-        println("Connecting to ${resources.getString(R.string.server_url)}")
-        Uri.parse(resources.getString(R.string.server_url))
-    }
+    private val uri by lazy { Uri.parse(resources.getString(R.string.server_url)) }
     private val reporterService by lazy { StatsRCP(uri) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,10 +70,9 @@ class MainActivity : ComponentActivity() {
 
 class StatsRCP(uri: Uri) : Closeable {
     val responseState = mutableStateOf("")
+    val responseValues = mutableStateOf(statsReply {  })
 
     private val channel = let {
-        println("Connecting to ${uri.host}:${uri.port}")
-
         val builder = ManagedChannelBuilder.forAddress(uri.host, uri.port)
         if (uri.scheme == "https") {
             builder.useTransportSecurity()
@@ -71,14 +85,16 @@ class StatsRCP(uri: Uri) : Closeable {
 
     private val reporter = StatsReportGrpcKt.StatsReportCoroutineStub(channel)
 
-    suspend fun sendRequest(name: String) {
+    suspend fun sendRequest(lqi: Float = 80F, lastMinutes: Int = 10) {
         try {
             val request = statsRequest {
-                this.name = name
-                this.lqi = 100F
+                this.name = "Stats from Android App"
+                this.lqi = lqi
+                this.timestamp = (Instant.now().epochSecond - lastMinutes * 60) * 1000000
             }
             val response = reporter.getStats(request)
-            responseState.value = response.toString()
+            responseState.value = "Received: ${response.statsCount} items."
+            responseValues.value = response
         } catch (e: Exception) {
             responseState.value = e.message ?: "Unknown Error"
             e.printStackTrace()
@@ -90,21 +106,112 @@ class StatsRCP(uri: Uri) : Closeable {
     }
 }
 
+var timeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+    .withZone(ZoneId.systemDefault())
+
+fun Float.format(digits: Int) = "%.${digits}f".format(this)
 @Composable
 fun Reporter(statsRCP: StatsRCP) {
     val scope = rememberCoroutineScope()
-    val nameState = remember { mutableStateOf(TextFieldValue()) }
-    Column(Modifier.fillMaxWidth().fillMaxHeight(), Arrangement.Top, Alignment.CenterHorizontally) {
-        Text(stringResource(R.string.name_hint), modifier = Modifier.padding(top = 10.dp))
-        OutlinedTextField(nameState.value, { nameState.value = it })
+    var lqi_text by rememberSaveable { mutableStateOf("20") }
+    var last_minutes by rememberSaveable { mutableStateOf("10") }
 
-        Button({ scope.launch { statsRCP.sendRequest(nameState.value.text) } }, Modifier.padding(10.dp)) {
-            Text(stringResource(R.string.send_request))
-        }
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(), Arrangement.Top, Alignment.CenterHorizontally) {
 
         if (statsRCP.responseState.value.isNotEmpty()) {
             Text(stringResource(R.string.server_response), modifier = Modifier.padding(top = 10.dp))
-            Text(statsRCP.responseState.value)
+            if (statsRCP.responseValues.value.statsCount == 0){
+                Text(statsRCP.responseState.value)
+            }
+            val gridItems = statsRCP.responseValues.value.statsMap.toList()
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(128.dp),
+
+                // content padding
+                contentPadding = PaddingValues(
+                    start = 12.dp,
+                    top = 16.dp,
+                    end = 12.dp,
+                    bottom = 16.dp
+                ),
+                content = {
+                    items(gridItems.size) { index ->
+                        val key = gridItems[index].first
+                        val value = gridItems[index].second
+                        val seqLostDupes = value.seqLost + value.seqDuplicates
+                        Card {
+                            Text(
+                                text = key,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 24.sp,
+                                color = Color.Unspecified,
+                                textAlign = TextAlign.Center,
+                                modifier = Modifier.padding(4.dp)
+                            )
+                            Text(
+                                text = "LQI: ${value.lqi.format(0)}",
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 16.sp,
+                                color = if (value.lqi < 60) Color.Red else Color.Unspecified,
+                                textAlign = TextAlign.Left,
+                                modifier = Modifier.padding(2.dp)
+                            )
+                            Text(
+                                text = "LOST: $seqLostDupes / ${value.seqCnt}",
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 16.sp,
+                                color = if (seqLostDupes > 5) Color.Red else if (seqLostDupes > 2) Color.Yellow else Color.Unspecified,
+                                textAlign = TextAlign.Left,
+                                modifier = Modifier.padding(2.dp)
+                            )
+                            Text(
+                                text = timeFormatter.format(Instant.ofEpochSecond(value.timestamp / 1000000)),
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 16.sp,
+                                color = Color.Unspecified,
+                                textAlign = TextAlign.Left,
+                                modifier = Modifier.padding(2.dp)
+                            )
+                        }
+                    }
+                }
+            )
+        } else {
+            Spacer(modifier = Modifier.height(32.dp))
         }
+
+        Button(
+            { scope.launch {
+                statsRCP.sendRequest(
+                    lqi_text.toFloat(),
+                    last_minutes.toInt())
+                }
+            },
+            Modifier.padding(10.dp)
+        ) {
+            Text(stringResource(R.string.send_request))
+        }
+        Row {
+            TextField(
+                value = lqi_text,
+                onValueChange = {
+                    lqi_text = it
+                },
+                label = { Text(stringResource(R.string.min_lqi)) },
+                modifier = Modifier.widthIn(min = 40.dp, max = 80.dp)
+            )
+            TextField(
+                value = last_minutes,
+                onValueChange = {
+                    last_minutes = it
+                },
+                label = { Text(stringResource(R.string.last_minutes)) },
+                modifier = Modifier.width(150.dp)
+            )
+        }
+
     }
 }
